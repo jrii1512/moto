8a1ec4a2a979c508a1a98d6364530aeed52da424
riimalaj <jriimala@gmail.com>
2022-12-22 13:48:47 +0200

    import addition to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//import { fcgi } from 'https://deno.land/x/fcgi@v0.0.21/mod.ts';
+import { fcgi } from 'https://deno.land/x/fcgi@v0.0.21/mod.ts';
 import {
     Application,
     HttpServerStd,
@@ -8,18 +8,6 @@ import {
 import routes from './routes/routes.js';
 import { configure } from './deps.js';
 import { listenAndServe } from 'https://deno.land/std@0.113.0/http/server.ts';
-
-
-/*
-console.log(`Started on [::1]:7775`);
-fcgi.listen('[::1]:7775', '', async (req) => {
-    console.log(req.url);
-    req.responseHeaders.set('Content-Type', 'text/html');
-    await req.respond({
-        body: 'Your cookies: ' + JSON.stringify([...req.cookies.entries()]),
-    });
-});
-*/
 
 configure({
     views: `${Deno.cwd()}/views/`,
